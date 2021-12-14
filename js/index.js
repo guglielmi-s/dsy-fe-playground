0d4f890fe231976bbac57cbf638a6de61964f1a7
sciuto-s <simone.s@docsity.com>
2021-12-14 14:41:08 +0100

    filtering array no RAMDA

--- js/index.js
+++ js/index.js
@@ -1,4 +1,4 @@
-import { format } from "date-fns";
+import { format, isBefore } from "date-fns";
 import * as R from "ramda";
 
 /* 
@@ -9,4 +9,23 @@ a quoted sequence, represent a 'real' single quote. (see the last example) */
 console.log(format(new Date(), "'Today is a' eeee"));
 console.log("prova")
 console.log(R.isNil([]));
+
+
+/* Js Excercies: Total price #2 */
+
+//Crea una funzione che calcoli il prezzo totale partendo da questo carrello di dati.
+
+
+
+ 
+function rangeStep(start, range, stop) {
+    var arr= []
+    for(let i=start; i<=stop; i+=range) {
+        arr.push(i)
+    }
+    console.log(arr)
+}
+rangeStep(10,2,21)
+
+
 
