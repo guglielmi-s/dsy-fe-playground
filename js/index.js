e4f1dbf7839806a6619417babf90d752feba57b1
sciuto-s <simone.s@docsity.com>
2021-12-14 11:38:04 +0100

    primo

--- js/index.js
+++ js/index.js
@@ -1,6 +1,12 @@
 import { format } from "date-fns";
 import * as R from "ramda";
 
+/* 
+The characters wrapped between two single quotes characters 
+(') are escaped. Two single quotes in a row, whether inside or outside 
+a quoted sequence, represent a 'real' single quote. (see the last example) */
+
 console.log(format(new Date(), "'Today is a' eeee"));
 
 console.log(R.isNil([]));
+
