e3b2fde066c11675090f571bbffa55344eef1fea
proietti-s <stefano.pro@docsity.com>
2021-12-15 12:54:41 +0100

    creazione array di numeri

--- js/index.js
+++ js/index.js
@@ -7,3 +7,9 @@ console.log(R.isNil([]));
 
 console.log('-------------Ramda: IsEven Exercise------------------------------')
 
+/* partendo da un array di numeri:
+- filtrare l'array mostrando solo i numeri pari
+- creare un nuovo array con i valori moltiplicati per numero */
+
+
+const numbers = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20]
