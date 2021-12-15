3445c7673133b64baae77c5a52c3a2b11b8fe1b5
proietti-s <stefano.pro@docsity.com>
2021-12-15 12:27:48 +0100

    creazione nuovo branch isEven

--- js/index.js
+++ js/index.js
@@ -4,3 +4,6 @@ import * as R from "ramda";
 console.log(format(new Date(), "'Today is a' eeee"));
 
 console.log(R.isNil([]));
+
+console.log('-------------Ramda: IsEven Exercise------------------------------')
+
