7f924beef9cb04e21d9eb4a3424ad2de5042db20
proietti-s <stefano.pro@docsity.com>
2021-12-16 18:45:45 +0100

    #5 addToDate

---
+++ js/exercise-5-addToDate.mjs
@@ -0,0 +1,25 @@
+import { format , addDays, parseISO } from "date-fns";
+
+import * as R from "ramda";
+
+
+
+/*
+ Scrivi una funzione che riceve due valori: 
+ una data (12/25/2021) e un numero intero, 
+ somma quel valore come giorni alla data.
+  */
+
+function aggGio (data, num){
+    let mydate = new Date()
+    mydate = data
+    const calc = addDays(data, num)
+
+    const formatDate = (data) => {
+    let x = format(data, 'MM/dd/yyyy');
+    return x}
+
+    return formatDate(calc)
+}
+
+console.log(aggGio(new Date(), 40))
