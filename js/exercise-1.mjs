343c229ad270bad79c302ce8f1a664b9743aee94
proietti-s <stefano.pro@docsity.com>
2021-12-16 17:46:41 +0100

    #5 add-to-date

--- js/exercise-1.mjs
+++ js/exercise-1.mjs
@@ -1,6 +1,15 @@
 import { format } from "date-fns";
 import * as R from "ramda";
 
-console.log(format(new Date(), "'Today is a' eeee"));
 
-console.log(R.isNil([]));
+
+/*
+ Scrivi una funzione che riceve due valori: una data (12/25/2021) e un numero intero, somma quel valore come giorni alla data.
+  */
+ function sumDay(data, v){
+    var myData = format(new Date(data), 'MM/dd/yyyy')
+    console.log(myData)
+     var result = addDays(myData, v)
+    console.log(result)
+}
+sumDay('08/01/1993', 20)
