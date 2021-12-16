c8e73c8dbd198deae7d9bcbd0ef5150448ee806a
proietti-s <stefano.pro@docsity.com>
2021-12-16 10:35:31 +0100

    creazione nuovo branch e array noteList

--- js/index.js
+++ js/index.js
@@ -1,77 +1,34 @@
 import * as R from "ramda";
 
-
-
-// exercise getDaysList //
-/* creare un metodo che riceve in input due valori: 
-month, year e ritorna il numero dei giorni di quel mese */
-
-/* var date = new Date();
-var month = getMonth(date);
-var year = getYear(date);
-
-console.log('mese =' + month + ' ' + 'anno = ' + year) */
-
-function getDaysList(month, year) {
-
-    /*if ((R.is(String, year) || R.is(String, month)) && (month < 1 || month > 12)&& */
-
-    if (year < 1492) {
-        console.log("Scrivi il mese o l'anno in cifre, ad es: Gennaio = 1");
-    } else {
-
-        var result = (0, _dateFns.getDaysInMonth)(new Date(year, month + 1));
-        return result;
-    }
-}
-console.log(getDaysList(1, 1235));
-
-
-console.log("************************ Crea una funzione che calcoli il prezzo totale partendo da questo carrello di dati ****************************************")
-
-
-
-const cart = [{
-    name: 'apples',
-    price: 2.49
-}, {
-    name: 'soap',
-    price: 1.99
-}, {
-    name: 'milk',
-    price: 2.99
-}, {
-    name: 'eggs',
-    price: 3.99
-}, {
-    name: 'carrots',
-    price: 2.99
-}, {
-    name: 'butter',
-    price: 1.49
-}, {
-    name: 'fish',
-    price: 9
-}, {
-    name: 'lettuce',
-    price: 2.99
-}, {
-    name: 'broccoli',
-    price: 4.99
-}, {
-    name: 'lemons',
-    price: 3.49
-}]
-
-function getTotalPrice(arr) {
-    var price = [];
-    const pushing = p => price.push(p.price)
-    R.map(pushing, arr)
-    const risultato = parseFloat(R.sum(price)).toFixed(2)
-    console.table(price)
-    return risultato + " €"
-}
-const prova = getTotalPrice(cart)
-console.log(prova)
-
-console.log('*****************************************')
+const notesList = [
+  {
+    title: "Buy milk and bread",
+    createdAt: "2020-04-04",
+    dueDate: null,
+    archived: false,
+  },
+  {
+    title: "Pick up a package at the post office",
+    createdAt: "2020-04-04",
+    dueDate: null,
+    archived: false,
+  },
+  {
+    title: "Take a walk with Yoda",
+    createdAt: "2020-04-04",
+    dueDate: null,
+    archived: true,
+  },
+  {
+    title: "Read 15 minutes",
+    createdAt: "2020-03-07",
+    dueDate: "2020-03-08",
+    archived: false,
+  },
+  {
+    title: "Do 30 minutes workout",
+    createdAt: "2020-03-01",
+    dueDate: "2020-03-04",
+    archived: false,
+  },
+];
