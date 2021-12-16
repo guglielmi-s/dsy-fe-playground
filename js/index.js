9a3c7281170b5de0659fde76b14c7f8706edbb6a
proietti-s <stefano.pro@docsity.com>
2021-12-16 12:35:42 +0100

    aggiornamento: modifica esercizio

--- js/index.js
+++ js/index.js
@@ -1,32 +1,6 @@
 import * as R from "ramda";
 
 
-
-// exercise getDaysList //
-/* creare un metodo che riceve in input due valori: 
-month, year e ritorna il numero dei giorni di quel mese */
-
-/* var date = new Date();
-var month = getMonth(date);
-var year = getYear(date);
-
-console.log('mese =' + month + ' ' + 'anno = ' + year) */
-
-function getDaysList(month, year) {
-
-    /*if ((R.is(String, year) || R.is(String, month)) && (month < 1 || month > 12)&& */
-
-    if (year < 1492) {
-        console.log("Scrivi il mese o l'anno in cifre, ad es: Gennaio = 1");
-    } else {
-
-        var result = (0, _dateFns.getDaysInMonth)(new Date(year, month + 1));
-        return result;
-    }
-}
-console.log(getDaysList(1, 1235));
-
-
 console.log("************************ Crea una funzione che calcoli il prezzo totale partendo da questo carrello di dati ****************************************")
 
 
@@ -61,9 +35,9 @@ const cart = [{
 }, {
     name: 'lemons',
     price: 3.49
-}]
+}];
 
-function getTotalPrice(arr) {
+/* function getTotalPrice(arr) {
     var price = [];
     const pushing = p => price.push(p.price)
     R.map(pushing, arr)
@@ -74,4 +48,13 @@ function getTotalPrice(arr) {
 const prova = getTotalPrice(cart)
 console.log(prova)
 
-console.log('*****************************************')
+console.log('*****************************************') */
+
+
+const prices = p => p.price 
+const priceOnly = R.map(prices, cart)
+const result = parseFloat(R.sum(priceOnly)).toFixed(2) + ' €';
+console.log(priceOnly)
+console.log('totale = '+ result)
+
+
