a5e87da2a070c45bfa4fc673fa2b8465342eba5b
proietti-s <stefano.pro@docsity.com>
2021-12-17 14:51:27 +0100

    #7 filterByKey

--- js/ex-6-lastNelements.mjs
+++ js/ex-6-lastNelements.mjs
@@ -2,42 +2,9 @@ import { format } from "date-fns";
 
 import * as R from "ramda";
 
-/* Creare una funzione che prenda un array di valori 
-e un numero x in input
-e torni gli ultimi x elementi.
-se l array ha meno elementi del valori di x, ritorna null
+/* Creare un metodo che riceve un oggetto con due proprietà 
+come argomento e deve ritornare il valore della proprietà 'prop-2'
 
-myFunction([1, 2, 3, 4, 5], 2)
-Expected [ 4, 5 ] */
+myFunction({  one: 1,  'prop-2': 2})
+Expected 2 */
 
-const numbers = [1,2,3,4,5,6,7,8,9,10]
-
-function lastElem(arr, num){
-
-    if(arr.length < num) {
-        return null
-    }
-    
-    const calc = R.slice(num, Infinity , arr);
-
-    return calc
-    
-}
-
-console.log(lastElem(numbers, 7))
-
-console.log('-------------variante con arr.length----------------')
-
-function lastElemLength(arr, num){
-
-    if(arr.length < num) {
-        return null
-    }
-    
-    const oper = R.slice(num, arr.length , arr);
-
-    return oper
-    
-}
-
-console.log(lastElemLength(numbers, 8))
