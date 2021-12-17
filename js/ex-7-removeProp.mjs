8b9c75c43fa101ef469b15920972d07ef2453a73
sciuto-s <simone.s@docsity.com>
2021-12-17 15:04:02 +0100

    omit

--- js/ex-7-removeProp.mjs
+++ js/ex-7-removeProp.mjs
@@ -2,8 +2,18 @@ import { format } from "date-fns";
 
 import * as R from "ramda";
 
-/* Scrivere un metodo che riceve in input un oggetto e ritorna lo stesso senza la proprietà con la  chiave 'country'
+/* Scrivere un metodo che riceve in input un oggetto e ritorna lo stesso senza la proprietà 
+con la  chiave 'country'
 
 myFunction({ continent: 'Asia', country: 'Japan', region: 'Kansai' })
 Expected { continent: 'Asia', region: 'Kansai' }*/
+
+const obj = { continent: 'Asia', country: 'Japan', region: 'Kansai' }
+
+function removeKey(myObj){
+   const remove= R.omit(['country'], myObj)
+   return remove
+}
+const omitted = removeKey(obj);
+console.log(omitted)
 
