d62db5b6a281dea2c1a39d3e2745f3501143be19
proietti-s <stefano.pro@docsity.com>
2021-12-17 14:54:13 +0100

    #7 removeProp

---
+++ js/ex-7-removeProp.mjs
@@ -0,0 +1,9 @@
+import { format } from "date-fns";
+
+import * as R from "ramda";
+
+/* Scrivere un metodo che riceve in input un oggetto e ritorna lo stesso senza la proprietà con la  chiave 'country'
+
+myFunction({ continent: 'Asia', country: 'Japan', region: 'Kansai' })
+Expected { continent: 'Asia', region: 'Kansai' }*/
+
