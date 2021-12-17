f164f090eab2223974272047f1ff5f987737bf97
proietti-s <stefano.pro@docsity.com>
2021-12-17 11:52:35 +0100

    #6 completed

--- js/ex-6-lastNelements.mjs
+++ js/ex-6-lastNelements.mjs
@@ -11,3 +11,33 @@ myFunction([1, 2, 3, 4, 5], 2)
 Expected [ 4, 5 ] */
 
 const numbers = [1,2,3,4,5,6,7,8,9,10]
+
+function lastElem(arr, num){
+
+    if(arr.length < num) {
+        return null
+    }
+    
+    const calc = R.slice(num, Infinity , arr);
+
+    return calc
+    
+}
+
+console.log(lastElem(numbers, 7))
+
+console.log('-------------variante con arr.length----------------')
+
+function lastElemLength(arr, num){
+
+    if(arr.length < num) {
+        return null
+    }
+    
+    const oper = R.slice(num, arr.length , arr);
+
+    return oper
+    
+}
+
+console.log(lastElemLength(numbers, 8))
