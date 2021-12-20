3daa696d7d8cc9126dd2c2474c855651a1111eef
sciuto-s <simone.s@docsity.com>
2021-12-20 14:08:17 +0100

    import it from "date-fns/locale/it/index.js"

--- js/exercise-4-giorno.mjs
+++ js/exercise-4-giorno.mjs
@@ -1,27 +1,10 @@
-import { format, getDay, toDate } from "date-fns";
-import * as R from "ramda";
+import { format } from "date-fns";
+import it from "date-fns/locale/it/index.js";
 
-
-/* Creare un metodo che riceve una data nel formato (MM/DD/YYYY) 
-e ritorna il giorno della settimana.
-esempio:
-getDay("12/07/2016") ➞ "Wednesday" */
-
-
-
-/* format(new Date(), 'EEEE') => restituisce giorno in lettere */
-
-/* format(new Date(2014, 1, 11), 'MM/dd/yyyy') => cambia formato orario*/
-
-/* const dayLetter = format(new Date(), 'EEEE');
-console.log(dayLetter)
-const dateMDY = format(new Date(), 'MM/dd/yyyy');
-console.log(dateMDY) */
-
-function getDays(data){
+function getDays(data) {
     const dateToday = format(data, 'MM/dd/yyyy')
     console.log(dateToday)
-    const myDay = format(data, 'EEEE')
+    const myDay = format(data, 'EEEE', { locale: it })
     return myDay
 }
 
