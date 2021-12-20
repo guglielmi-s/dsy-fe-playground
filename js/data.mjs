57dea339b0066242e65d66c95ed8fbd48edba60a
Stefano Guglielmi <stefano.g@docsity.com>
2021-12-20 10:34:25 +0100

    aggiunto export dell'oggetto in un file nuovo

---
+++ js/data.mjs
@@ -0,0 +1,3 @@
+const obj = { continent: "Asia", country: "Japan", region: "Kansai" };
+
+export default obj;
--- js/ex-7-removeProp.mjs
+++ js/ex-7-removeProp.mjs
@@ -2,18 +2,17 @@ import { format } from "date-fns";
 
 import * as R from "ramda";
 
+import obj from "./data.mjs";
+
 /* Scrivere un metodo che riceve in input un oggetto e ritorna lo stesso senza la proprietà 
 con la  chiave 'country'
 
 myFunction({ continent: 'Asia', country: 'Japan', region: 'Kansai' })
 Expected { continent: 'Asia', region: 'Kansai' }*/
 
-const obj = { continent: 'Asia', country: 'Japan', region: 'Kansai' }
-
-function removeKey(myObj){
-   const remove= R.omit(['country'], myObj)
-   return remove
+function removeKey(myObj) {
+  const remove = R.omit(["country"], myObj);
+  return remove;
 }
 const omitted = removeKey(obj);
-console.log(omitted)
-
+console.log(omitted);
