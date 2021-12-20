2eeb6f99fbf9cd27f2257d5a76f6d34801b52467
proietti-s <stefano.pro@docsity.com>
2021-12-20 12:08:51 +0100

    esercizio corretto

--- js/ex-6-lastNelements.mjs
+++ js/ex-6-lastNelements.mjs
@@ -1,4 +1,4 @@
-import { format } from "date-fns";
+import numbers from "./data.mjs"
 
 import * as R from "ramda";
 
@@ -10,34 +10,17 @@ se l array ha meno elementi del valori di x, ritorna null
 myFunction([1, 2, 3, 4, 5], 2)
 Expected [ 4, 5 ] */
 
-const numbers = [1,2,3,4,5,6,7,8,9,10]
-
 function lastElem(arr, num){
 
     if(arr.length < num) {
         return null
     }
     
-    const calc = R.slice(num, Infinity , arr);
+    const calc = R.slice(arr.length - num , arr.length, arr);
 
     return calc
     
 }
 
-console.log(lastElem(numbers, 7))
+console.log(lastElem(numbers, 8))
 
-console.log('-------------variante con arr.length----------------')
-
-function lastElemLength(arr, num){
-
-    if(arr.length < num) {
-        return null
-    }
-    
-    const oper = R.slice(num, arr.length , arr);
-
-    return oper
-    
-}
-
-console.log(lastElemLength(numbers, 8))
