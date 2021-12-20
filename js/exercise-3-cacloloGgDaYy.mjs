4ce0b487a9169dafaad2d95deb9a123728190ce6
sciuto-s <simone.s@docsity.com>
2021-12-20 10:22:16 +0100

    getDaysFromYear

--- js/exercise-3-cacloloGgDaYy.mjs
+++ js/exercise-3-cacloloGgDaYy.mjs
@@ -9,9 +9,9 @@ calcAge(65) ➞ 23725
 calcAge(20) ➞ 7300
 */
 
-function calcAge(n) {
+function getDaysFromYear(n) {
     const result = (getDaysInYear(new Date()) * n)
     return result
 }
-const p = calcAge(20);
+const p = getDaysFromYear(20);
 console.log(p)
