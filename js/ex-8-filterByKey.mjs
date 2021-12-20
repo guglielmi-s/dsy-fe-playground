6c8676b062456d2ad6a77cf7451df75830e91e15
proietti-s <stefano.pro@docsity.com>
2021-12-20 11:03:46 +0100

    aggiunto modulo data.mjs e modificato nome function

--- js/ex-8-filterByKey.mjs
+++ js/ex-8-filterByKey.mjs
@@ -1,6 +1,5 @@
-import { format } from "date-fns";
-
 import * as R from "ramda";
+import obj from "./data.mjs";
 
 /* Creare un metodo che riceve un oggetto con due proprietà 
 come argomento e deve ritornare il valore della proprietà 'prop-2'
@@ -8,11 +7,9 @@ come argomento e deve ritornare il valore della proprietà 'prop-2'
 myFunction({  one: 1,  'prop-2': 2})
 Expected 2 */
 
-const _obj = {a: 1,  b: 2, c: 3, d:4}
-
-function filterKey (obj){
-    const result = R.prop(['b'], obj);
+function getValueByKey (obj){
+    const result = R.prop("b", obj);
     return result
 }
 
-console.log(filterKey(_obj))
+console.log(getValueByKey(obj))
