5fe359d1e6b1e24516bb1a7c3b4e80d9316192dc
proietti-s <stefano.pro@docsity.com>
2021-12-21 18:07:50 +0100

    esercizio aggiornato

--- js/es-2-arrayMultiply.mjs
+++ js/es-2-arrayMultiply.mjs
@@ -9,12 +9,21 @@ creare funzione che riceve due numeri come argomenti (num, length) e ritorna un 
 arrayOfMultiples(7, 5) ➞ [7, 14, 21, 28, 35]
  */
 
-const arrayOfMultiples = (a, b) => {
+/* const arrayOfMultiples = (a, b) => {
   var multiples = [];
   for (let i = 1; i <= b; i++) {
     multiples.push(a * i)
   }
   console.table({ multiples })
 }
-arrayOfMultiples(9, 10)
+arrayOfMultiples(9, 10) */
 
+
+function arrOfMulty (start, end) {
+  const myArr = Array(end).fill().map((elem, i) => (i+1) * start);
+  return myArr
+}
+
+console.log(arrOfMulty(7, 5))
+console.log(arrOfMulty(8,3))
+console.table(arrOfMulty(6,6))
