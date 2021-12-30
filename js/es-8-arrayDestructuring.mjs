28a1b413fc859cc6434f70f03dffebc93f60962d
sciuto-s <simone.s@docsity.com>
2021-12-30 15:43:46 +0100

    es8 aggiornato

--- js/es-8-arrayDestructuring.mjs
+++ js/es-8-arrayDestructuring.mjs
@@ -4,7 +4,5 @@
 // usando il destructuring valorizzare una nuova variabile con questo valore: [2,3,4]
 
 const numbers = [1, 2, 3, 4]
-let newNumber = [];
-const [, b, c, d] = numbers
-newNumber = [b, c, d];
-console.log(newNumber);
+const [ ,...b] = numbers
+console.log(b);
