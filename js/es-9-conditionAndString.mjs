a882a5c76c8c9eb8449f3e33fb04ba06c5f7317b
Simone Sciuto <simoSciuto@MacBook-Pro-di-Simone.local>
2022-01-03 12:24:12 +0100

    prova correzione

--- js/es-9-conditionAndString.mjs
+++ js/es-9-conditionAndString.mjs
@@ -2,10 +2,20 @@
 
 // sayHelloBye("alon", 1) ➞ "Hello Alon"
 
-const sayHello = (name, num) => {
-    num === 1 && console.log(`Hello ${name[0].toUpperCase()}${name.slice(1)}`);
-    num === 0 && console.log(`Bye ${name[0].toUpperCase()}${name.slice(1)}`);
-    // c'è sicuramente un altro modo per non ricopiare tutto il codice 
+// const sayHello = (name, num) => {
+//     num === 1 && console.log(`Hello ${name[0].toUpperCase()}${name.slice(1)}`);
+//     num === 0 && console.log(`Bye ${name[0].toUpperCase()}${name.slice(1)}`);
+//     // c'è sicuramente un altro modo per non ricopiare tutto il codice 
     
+// }
+// sayHello('adolfo', 1)
+
+const toUpper = (str) =>{
+    return `${str[0].toUpperCase()}${str.slice(1)}`
 }
-sayHello('adolfo', 1)
+const sayHello =(name, num) =>{
+   return  num === 0 ?  `Bye ${toUpper(name)}` : `Hello ${toUpper(name)}`
+}
+console.log(sayHello('adolfo',1))
+
+
